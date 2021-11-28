d81e88dd83ced639a063eab8b90f201daf62d22f
Mykhailo Skliar <mykhailo.skliar@c-com.net>
2021-11-28 18:53:24 +0100

    finished implementation of infrastructure with webhooks, started implementation of infrastructure with tests

--- gcp/packer/worker/setup.sh
+++ gcp/packer/worker/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Install Java JDK 8"
+echo "Install Java JDK 11"
 yum remove -y java
 yum install -y java-11-openjdk-devel
 
@@ -8,10 +8,6 @@ echo "Install Docker engine"
 yum update -y
 yum install docker -y
 usermod -aG docker ec2-user
-reboot
-systemctl unmask docker.service
-systemctl unmask docker.socket
-systemctl start docker.service
 systemctl enable docker
 
 echo "Install git"
